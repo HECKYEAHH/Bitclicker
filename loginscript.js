d19884e84541dd243d34a39058ebba3a7b60bad8
HECKYEAHH <rj3noone@gmail.com>
2024-06-27 21:15:51 -0400

    v0.52
    Added Username functionality to move to idle game top bar menu

--- loginscript.js
+++ loginscript.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('user-form');
 
   form.addEventListener('submit', function(event) {
+      event.preventDefault();
       const userName = document.getElementById('user-name').value;
       if (!userName) {
           alert('Please enter your user-name.');
-          event.preventDefault();
+      } else {
+          localStorage.setItem('username', userName);
+          window.location.href = 'idle.html'; // Redirect to the BitClicker game page
       }
   });
 });
